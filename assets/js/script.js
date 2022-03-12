28257fd5f1fd6fd5fb793277e73970dd4495d2d1
Sophia Davis <sophiadavis@Sophias-MBP.attlocal.net>
2022-03-12 13:39:56 -0500

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,20 @@ $(document).ready(function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-  
+        //local storage for items
+        localStorage.setItem(time, text);
+    })
+    //pull data from local storage at every hour
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+
+})
